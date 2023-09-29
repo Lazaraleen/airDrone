613788acd464595b05fd2a0bd318b25184ecd31a
Myriam JAYLE <myriamjayle@gmail.com>
2023-09-29 10:03:40 +0200

    Chacune des données modifiables est modifiable, une par une

--- app3.js
+++ app3.js
@@ -1,42 +1,53 @@
 const response2 = await fetch("drones.json");
 const dronesJSON = await response2.json();
 
-const drones = dronesJSON;
-localStorage.setItem("drones", JSON.stringify(drones));
+let drones = dronesJSON;
+// localStorage.setItem("drones", JSON.stringify(drones));
+if (localStorage !== null) {
+  const storedDrones = localStorage.getItem("drones");
+
+  if (storedDrones !== null) {
+    drones = JSON.parse(storedDrones);
+  }
+}
 
 // **************************************************************** Liste des Drones Actifs *****************************************************
 
 const listeActifs = document.querySelector(".listeDrones");
 
-for (let i = 0; i < drones.length; i++) {
-  const dataDrone = document.createElement("div");
-  const datas = document.createElement("div");
-  dataDrone.classList.add("datadrone");
-  datas.classList.add("datas2");
+function listeDrone() {
+  for (let i = 0; i < drones.length; i++) {
+    const dataDrone = document.createElement("div");
+    const datas = document.createElement("div");
+    dataDrone.classList.add("datadrone");
+    datas.classList.add("datas2");
 
-  const nameDrone = document.createElement("p");
-  const speed = document.createElement("p");
-  const depart = document.createElement("p");
-  const arrivee = document.createElement("p");
-  const actif = document.createElement("p");
+    const nameDrone = document.createElement("p");
+    const speed = document.createElement("p");
+    const depart = document.createElement("p");
+    const arrivee = document.createElement("p");
+    const actif = document.createElement("p");
 
-  nameDrone.innerText = drones[i].name;
-  speed.innerText = drones[i].speed / 1000 + " secondes";
-  depart.innerText = drones[i].depart;
-  arrivee.innerText = drones[i].arrivee;
-  actif.innerText = drones[i].actif;
-  drones[i].actif === "YES"
-    ? (actif.style.color = "green")
-    : (actif.style.color = "red");
+    nameDrone.innerText = drones[i].name;
+    speed.innerText = drones[i].speed / 1000 + " secondes";
+    depart.innerText = drones[i].depart;
+    arrivee.innerText = drones[i].arrivee;
+    actif.innerText = drones[i].actif;
+    drones[i].actif === "YES"
+      ? (actif.style.color = "green")
+      : (actif.style.color = "red");
 
-  datas.appendChild(nameDrone);
-  datas.appendChild(speed);
-  datas.appendChild(depart);
-  datas.appendChild(arrivee);
-  datas.appendChild(actif);
-  dataDrone.appendChild(datas);
-  listeActifs.appendChild(dataDrone);
+    datas.appendChild(nameDrone);
+    datas.appendChild(speed);
+    datas.appendChild(depart);
+    datas.appendChild(arrivee);
+    datas.appendChild(actif);
+    dataDrone.appendChild(datas);
+    listeActifs.appendChild(dataDrone);
+  }
 }
+
+listeDrone();
 
 // **************************************************************** Modale *****************************************************
 
@@ -97,55 +108,131 @@ divDrone.classList.add("divDrone");
 modalbox.appendChild(image);
 modalbox.appendChild(divDrone);
 
-drones.forEach((e) => {
-  const droneButton = document.createElement("button");
-  droneButton.innerText = e.name;
-  droneButton.classList.add("droneButton");
-  divDrone.appendChild(droneButton);
-
-  droneButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    divDrone.innerHTML = "";
-    modif(e);
+function buttonDrone() {
+  drones.forEach((e) => {
+    const droneButton = document.createElement("button");
+    droneButton.innerText = e.name;
+    droneButton.classList.add("droneButton");
+    divDrone.appendChild(droneButton);
+  
+    droneButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      divDrone.innerHTML = "";
+      modif(e);
+    });
+  
+    originalDroneButtons.push(droneButton);
   });
+}
 
-  originalDroneButtons.push(droneButton);
-});
+buttonDrone();
 
 function modif(event) {
-  const selectedDrone = drones.find((drone) => drone.name === event.target.innerText);
+  const selectedDrone = drones.find(
+    (drone) => drone.name === event.target.innerText
+  );
   const id = selectedDrone.id;
 
   const divItem = document.createElement("div");
   const h3Name = document.createElement("h3");
   const h3Speed = document.createElement("h3");
-  // const h3Navette = document.createElement("h3");
   const h3Actif = document.createElement("h3");
   const formName = document.createElement("form");
   const formSpeed = document.createElement("form");
   const formActif = document.createElement("form");
+  const inputName = document.createElement("input");
+  const buttonName = document.createElement("button");
+  const inputSpeed = document.createElement("input");
+  const buttonSpeed = document.createElement("button");
+  const inputActif = document.createElement("input");
+  const buttonActif = document.createElement("button");
 
   h3Name.innerText = "Name : " + selectedDrone.name;
-  formName.innerHTML = `<input type="text" class="formVar" id="lname" value=${selectedDrone.name}><br><button type="submit" id="newName" class="submitButton">Submit</button>`;
+  formName.classList.add("form");
+  inputName.type = "text";
+  inputName.classList.add("formVar");
+  inputName.setAttribute("id", "lname");
+  buttonName.classList.add("submitButton");
+  buttonName.setAttribute("id", "newNameButton");
+  buttonName.innerText = "Submit";
+  // formName.innerHTML = `<input type="text" class="formVar" id="lname">
+  //     <button id="newNameButton" class="submitButton">Submit</button>`;
   h3Speed.innerText = "Speed : " + selectedDrone.speed + " s";
-  formSpeed.innerHTML = `<input type="text" class="formVar" id="lspeed" value=${selectedDrone.speed}><br><input type="submit" value="Submit" class="submitButton">`;
-  // h3Navette.innerText = "Navette : " + selectedDrone.navette;
+  formSpeed.classList.add("form");
+  inputSpeed.type = "text";
+  inputSpeed.classList.add("formVar");
+  inputSpeed.setAttribute("id", "lspeed");
+  buttonSpeed.classList.add("submitButton");
+  buttonSpeed.setAttribute("id", "newSpeedButton");
+  buttonSpeed.innerText = "Submit";
+  // formSpeed.innerHTML = `<input type="text" class="formVar" id="lspeed"><br><input type="submit" value="Submit" class="submitButton">`;
   h3Actif.innerText = "Actif ? : " + selectedDrone.actif;
-  formActif.innerHTML = `<input type="text" class="formVar" id="lactif" value=${selectedDrone.actif}><br><input type="submit" value="Submit" class="submitButton">`;
-
-  // const newName = document.getElementById('newName');
-  // const lname = document.getElementById('lname');
-
-  // newName.onclick = function(){
-  //   drones[id].name = lname.value;
-  // };
+  formActif.classList.add("form");
+  inputActif.type = "text";
+  inputActif.classList.add("formVar");
+  inputActif.setAttribute("id", "lactif");
+  buttonActif.classList.add("submitButton");
+  buttonActif.setAttribute("id", "newActifButton");
+  buttonActif.innerText = "Submit";
+  // formActif.innerHTML = `<input type="text" class="formVar" id="lactif"><br><input type="submit" value="Submit" class="submitButton">`;
 
   divItem.appendChild(h3Name);
+  formName.appendChild(inputName);
+  formName.appendChild(buttonName);
   divItem.appendChild(formName);
   divItem.appendChild(h3Speed);
+  formSpeed.appendChild(inputSpeed);
+  formSpeed.appendChild(buttonSpeed);
   divItem.appendChild(formSpeed);
-  // divItem.appendChild(h3Navette);
   divItem.appendChild(h3Actif);
+  formActif.appendChild(inputActif);
+  formActif.appendChild(buttonActif);
   divItem.appendChild(formActif);
   divDrone.appendChild(divItem);
+
+  let newNameButton = document.getElementById("newNameButton");
+  const lname = document.getElementById("lname");
+
+  newNameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newName = lname.value;
+    drones[id].name = newName;
+    h3Name.innerText = "Nom : " + newName;
+    localStorage.setItem("drones", JSON.stringify(drones));
+    divDrone.innerHTML = "";
+    buttonDrone();
+    listeActifs.innerHTML = "";
+    listeDrone();
+  });
+
+  let newSpeedButton = document.getElementById("newSpeedButton");
+  const lspeed = document.getElementById("lspeed");
+
+  newSpeedButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newSpeed = lspeed.value;
+    drones[id].speed = newSpeed;
+    h3Speed.innerText = "Nom : " + newSpeed;
+    localStorage.setItem("drones", JSON.stringify(drones));
+    divDrone.innerHTML = "";
+    buttonDrone();
+    listeActifs.innerHTML = "";
+    listeDrone();
+  });
+
+  let newActifButton = document.getElementById("newActifButton");
+  const lactif = document.getElementById("lactif");
+
+  newActifButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newActif = lactif.value;
+    drones[id].actif = newActif;
+    h3Actif.innerText = "Nom : " + newActif;
+    localStorage.setItem("drones", JSON.stringify(drones));
+    divDrone.innerHTML = "";
+    buttonDrone();
+    listeActifs.innerHTML = "";
+    listeDrone();
+  });
+
 }
